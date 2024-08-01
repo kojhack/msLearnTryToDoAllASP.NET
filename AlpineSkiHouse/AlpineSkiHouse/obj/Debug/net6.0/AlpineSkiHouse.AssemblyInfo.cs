bcb2b23930f00079ef327604878fa61ee32e19a1
kojhack <122357318+kojhack@users.noreply.github.com>
2024-08-01 07:47:34 +0000

    modified

--- AlpineSkiHouse/AlpineSkiHouse/obj/Debug/net6.0/AlpineSkiHouse.AssemblyInfo.cs
+++ AlpineSkiHouse/AlpineSkiHouse/obj/Debug/net6.0/AlpineSkiHouse.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlpineSkiHouse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0717d37e20198b84fcb1d419ee907095ca1eb6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a249c39eb5464fa5b93f2d827cd553f8d1f6a933")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlpineSkiHouse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlpineSkiHouse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
