a6bad13400560339b94f1cca2f972960b9a53c92
kojhack <122357318+kojhack@users.noreply.github.com>
2024-08-01 15:02:47 +0000

    modified

--- ContosoPizza/obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
+++ ContosoPizza/obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a249c39eb5464fa5b93f2d827cd553f8d1f6a933")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcb2b23930f00079ef327604878fa61ee32e19a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
