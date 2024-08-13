ac0352adae2d0d55cefac4de465aab8beb73aea2
kojhack <122357318+kojhack@users.noreply.github.com>
2024-08-13 17:44:50 +0000

    last push of the day

--- AlpineSkiHouse/AlpineSkiHouse/obj/Debug/net6.0/AlpineSkiHouse.AssemblyInfo.cs
+++ AlpineSkiHouse/AlpineSkiHouse/obj/Debug/net6.0/AlpineSkiHouse.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlpineSkiHouse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcb2b23930f00079ef327604878fa61ee32e19a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6bad13400560339b94f1cca2f972960b9a53c92")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlpineSkiHouse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlpineSkiHouse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
