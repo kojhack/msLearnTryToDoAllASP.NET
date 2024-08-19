7f71dd7553e9ffa2113b11a6f1ceb2cd7d6ed7a4
kojhack <122357318+kojhack@users.noreply.github.com>
2024-08-19 08:32:59 +0000

    Working with API

--- ContosoPizza/obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
+++ ContosoPizza/obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+487d8f6299153861fa5a6b12ffa343226b9dbd84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a12ec77783ba7e378396dca23e2d209586d3846")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
